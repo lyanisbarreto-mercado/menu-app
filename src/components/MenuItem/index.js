05eb0bb124eae61d565e3f98f8face063e9a63c8
Lyanis Barreto <lyanis.barreto@gmail.com>
2026-02-20 19:55:57 -0500

    Fixed the itemId issue, wehre it didn't appear properly

--- src/components/MenuItem/index.js
+++ src/components/MenuItem/index.js
@@ -2,13 +2,13 @@ import React from "react"
 
 const MenuItem = ({itemPrice, itemName, itemDescription, itemId}) => { //pass each property as a prop
         return (
-                <div class="menu-item" id={itemId}>
-                        <span>${itemPrice}</span>
+                <div className="menu-item" id={`menu-item-${itemId}`}>
+                        <span>${itemPrice}</span> 
                         <h2>{itemName}</h2>
                         <p>{itemDescription}</p>
                         <button>Add to Cart</button>
                     </div>
     )
 };
-
+//The dollar sign is passed as a string next to the itemPrice prop, so it still appears
 export default MenuItem; 
